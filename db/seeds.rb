9c8a70ed94657a014ea363525829782b26164298
nathanpena <nnpena@outlook.com>
2014-12-02 22:54:25 -0500

    Associated users to posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,20 @@
-# Create Posts
+require 'faker'
+
+ # Create Users
+ 5.times do
+   user = User.new(
+     name:     Faker::Name.name,
+     email:    Faker::Internet.email,
+     password: Faker::Lorem.characters(10)
+   )
+   user.skip_confirmation!
+   user.save!
+ end
+ users = User.all
+ 
  50.times do
    Post.create!(
+     user: users.sample,
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
@@ -15,7 +29,16 @@
    )
  end
  
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+ user = User.first
+ user.skip_reconfirmation!
+ user.update_attributes!(
+   email: 'nnpena@outlook.com',
+   password: 'poptart5'
+ )
+
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
  
