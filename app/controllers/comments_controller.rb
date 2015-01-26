c4dc6cf5da05fcbf28121a5a90cb567fb5fc2505
nathanpena <nnpena@outlook.com>
2015-01-25 21:55:52 -0500

    added destroy options

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
+  
   def create
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.user_id = current_user.id
+    authorize @comment
     if @comment.save
       redirect_to [@topic, @post], notice: "Comment saved successfully."
     else
@@ -11,6 +13,20 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+     @topic = Topic.find(params[:topic_id])
+     @post = @topic.posts.find(params[:post_id])
+     @comment = @post.comments.find(params[:id])
+ 
+     authorize @comment
+     if @comment.destroy
+       flash[:notice] = "Comment was removed."
+       redirect_to [@topic, @post]
+     else
+       flash[:error] = "Comment couldn't be deleted. Try again."
+       redirect_to [@topic, @post]
+     end
+   end
 
   private
 
