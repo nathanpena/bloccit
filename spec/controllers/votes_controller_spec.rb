b14ec1a28400d4d44238a2961b59e439f18e8991
nathanpena <nnpena@outlook.com>
2015-01-31 17:54:47 -0500

    Refactor Vote conotroller spec file

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -4,16 +4,27 @@
  
    include TestFactories
    include Devise::TestHelpers
+
+   before do
+    request.env["HTTP_REFERER"] = '/'
+    @user = authenticated_user
+    @post = associated_post
+    sign_in @user
+   end
+
    describe '#up_vote' do
-     it "adds an up-vote to the post" do
-      request.env["HTTP_REFERER"] = '/'
-       @user = authenticated_user
-       @post = associated_post
-       sign_in @user
- 
+     it "adds an up-vote to the post" do 
        expect {
          post( :up_vote, post_id: @post.id )
        }.to change{ @post.up_votes }.by 1
      end
    end
+
+   describe '#down_vote' do
+    it "adds a down-vote to the post" do
+      expect {
+        post( :down_vote, post_id: @post.id)
+      }.to change{ @post.down_votes }.by 1
+    end
+  end
  end
